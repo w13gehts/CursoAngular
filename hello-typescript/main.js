930719204c4d413b176a8c7f9effe0ca4de1c6af
Juliana <juliana.salafia@gmail.com>
2021-05-31 19:32:17 -0300

    Introdução ao TypeScript

---
+++ hello-typescript/main.js
@@ -0,0 +1,23 @@
+var minhaVar = 'minha variavel';
+function minhaFunc(x, y) {
+    return x + y;
+}
+//ES6 ou ES2015
+var num = 2;
+var PI = 3.14;
+var numeros = [1, 2, 3];
+numeros.map(function (valor) {
+    return valor * 2;
+});
+numeros.map(function (valor) { return valor * 2; }); //ES2015
+// =============================================== // 
+var Matematica = /** @class */ (function () {
+    function Matematica() {
+    }
+    Matematica.prototype.soma = function (x, y) {
+        return x + y;
+    };
+    return Matematica;
+}());
+var n1 = 'string';
+n1 = 4;
