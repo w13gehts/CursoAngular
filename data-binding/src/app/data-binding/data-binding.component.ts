986e6457cce715ad22bfe21916c5c88adf642401
Juliana <juliana.salafia@gmail.com>
2021-06-02 14:32:53 -0300

    Property Binding e Interpolação

---
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-binding',
+  templateUrl: './data-binding.component.html',
+  styleUrls: ['./data-binding.component.scss']
+})
+export class DataBindingComponent implements OnInit {
+  url: string = 'http://loiane.training';
+  cursoAngular: boolean = true;
+  urlImagem = 'http://lorempixel.com/g/400/200/nature/';
+
+  getValor() {
+    return 1;
+  }
+
+  getCurtirCurso() {
+    return true;
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
