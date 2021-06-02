b5cfaa36c119a78c87da8da409a60d55812abbd2
Juliana <juliana.salafia@gmail.com>
2021-06-02 14:02:54 -0300

    Introdução a Services e DI

--- primeiro-projeto/src/app/cursos/cursos.component.ts
+++ primeiro-projeto/src/app/cursos/cursos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CursosService } from './cursos.service';
 
 @Component({
   selector: 'app-cursos',
@@ -8,13 +9,16 @@ import { Component, OnInit } from '@angular/core';
 export class CursosComponent implements OnInit {
 
   nomePortal: string;
-  cursos: string [] = ['Java', 'Ext JS', 'Angular'];
+  cursos: string [];
 
-  constructor() { 
+  constructor(private cursosService: CursosService) { 
     this.nomePortal = 'http://loiane.training';
+
+    this.cursos = this.cursosService.getCursos();
+
   }
 
   ngOnInit(): void {
-  }
 
+  }
 }
